66fcb3b3458455a3b53c69d539af2a4fbae06556
Rene Hexel <r.hexel@griffith.edu.au>
2020-08-30 11:37:58 +1000

    Use local repository

--- Package.swift
+++ Package.swift
@@ -8,8 +8,10 @@ let package = Package(
         .library(name: "GdkPixbuf", targets: ["GdkPixbuf"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/rhx/SwiftGIO.git", .branch("master")),
-        .package(url: "https://github.com/rhx/SwiftGModule.git", .branch("master")),
+        .package(url: "file:///Users/rh/src/swift/rh/gtk/SwiftGIO", .branch("master")),
+        .package(url: "file:///Users/rh/src/swift/rh/gtk/SwiftGModule", .branch("master")),
+        //.package(url: "https://github.com/rhx/SwiftGIO.git", .branch("master")),
+        //.package(url: "https://github.com/rhx/SwiftGModule.git", .branch("master")),
     ],
     targets: [
 	.systemLibrary(name: "CGdkPixbuf", pkgConfig: "cairo glib-2.0 gio-unix-2.0",
