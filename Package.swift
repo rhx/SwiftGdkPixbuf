5337abbb820a64239206e29ddf61d2e6de870aa2
Rene Hexel <r.hexel@griffith.edu.au>
2020-08-31 08:00:53 +1000

    Note Swift 5.2 requirement

--- Package.swift
+++ Package.swift
@@ -1,17 +1,13 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.2
 
 import PackageDescription
 
 let package = Package(
     name: "GdkPixbuf",
-    products: [
-        .library(name: "GdkPixbuf", targets: ["GdkPixbuf"]),
-    ],
+    products: [ .library(name: "GdkPixbuf", targets: ["GdkPixbuf"]) ],
     dependencies: [
-        .package(url: "file:///Users/rh/src/swift/rh/gtk/SwiftGIO", .branch("master")),
-        .package(url: "file:///Users/rh/src/swift/rh/gtk/SwiftGModule", .branch("master")),
-        //.package(url: "https://github.com/rhx/SwiftGIO.git", .branch("master")),
-        //.package(url: "https://github.com/rhx/SwiftGModule.git", .branch("master")),
+        .package(name: "GIO", url: "https://github.com/rhx/SwiftGIO.git", .branch("master")),
+        .package(name: "GModule", url: "https://github.com/rhx/SwiftGModule.git", .branch("master")),
     ],
     targets: [
 	.systemLibrary(name: "CGdkPixbuf", pkgConfig: "cairo glib-2.0 gio-unix-2.0",
